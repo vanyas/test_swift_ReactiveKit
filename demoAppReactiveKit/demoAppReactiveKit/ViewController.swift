48b095cadd522390e211f23679452e84207e44d2
Vangeli Ontiveros Islas <vanyasescom@gmail.com>
2016-09-25 16:31:53 -0500

    Added Demo Test Project

---
+++ demoAppReactiveKit/demoAppReactiveKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  demoAppReactiveKit
+//
+//  Created by Vangeli Ontiveros Islas on 25/09/16.
+//  Copyright © 2016 Vangeli Ontiveros Islas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
